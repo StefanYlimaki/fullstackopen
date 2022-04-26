0e174e69d51ba70419de37cf80ae2d8573484cdb
stefan <stefan.ylimaki@gmail.com>
2022-04-26 22:23:29 +0300

    1.6 done

---
+++ osa1/unicafe/src/App.js
@@ -0,0 +1,40 @@
+import {useState} from 'react'
+
+const Display = ({good, neutral, bad}) => {
+  return(
+    <div>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
+  )
+}
+
+const Button = ({handleClick, text}) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodClick = () => setGood(good + 1)
+  const handleNeutralClick = () => setNeutral(neutral + 1)
+  const handleBadClick = () => setBad(bad + 1)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={handleGoodClick} text="good" />
+      <Button handleClick={handleNeutralClick} text="neutral" />
+      <Button handleClick={handleBadClick} text="bad" />
+      <h1>statictics</h1>
+      <Display good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+export default App
