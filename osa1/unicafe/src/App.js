f9658085177b228b869618ba816a37711272b195
stefan <stefan.ylimaki@gmail.com>
2022-04-26 22:43:48 +0300

    1.9 done

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,15 +1,24 @@
 import {useState} from 'react'
 
 const Statistics = ({good, neutral, bad}) => {
+  let sum = good+neutral+bad
+  if (sum != 0) {
+    return(
+      <div>
+        <h1>statistics</h1>
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+        <p>all {good+neutral+bad}</p>
+        <p>average {(good-bad)/(good+neutral+bad)}</p>
+        <p>positive {good/(good+neutral+bad)*100}</p>
+      </div>
+    )
+  }
   return(
     <div>
       <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {good+neutral+bad}</p>
-      <p>average {(good-bad)/(good+neutral+bad)}</p>
-      <p>positive {good/(good+neutral+bad)*100}</p>
+      <p>No feedback given</p>
     </div>
   )
 }
