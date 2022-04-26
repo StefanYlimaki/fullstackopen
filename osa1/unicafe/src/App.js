3cf7dbc87d3549af5ef86377a90b072f6d59543f
stefan <stefan.ylimaki@gmail.com>
2022-04-26 23:21:06 +0300

    1.11 done

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,23 +1,36 @@
 import {useState} from 'react'
 
 const StatisticLine = ({text, value}) => {
+  // For the percent of posivive feedbacks, we need to add "%" in the end.
+  if (text == "positive"){
+    return(
+      <tr>
+        <td>{text}</td>
+        <td>{value} %</td>
+      </tr>
+    )
+  }
   return(
-    <p>{text} {value}</p>
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
   )
 }
 
 const Statistics = ({good, neutral, bad}) => {
-  let sum = good+neutral+bad
   if ((good+neutral+bad) != 0) {
     return(
-      <div>
-        <StatisticLine text="good" value={good} />
-        <StatisticLine text="neutral" value={neutral} />
-        <StatisticLine text="bad" value={bad} />
-        <StatisticLine text="all" value={good+neutral+bad} />
-        <StatisticLine text="average" value={(good-bad)/(good+neutral+bad)} />
-        <StatisticLine text="positive" value={good/(good+neutral+bad)*100} />
-      </div>
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="all" value={good+neutral+bad} />
+          <StatisticLine text="average" value={(good-bad)/(good+neutral+bad)} />
+          <StatisticLine text="positive" value={good/(good+neutral+bad)*100} />
+        </tbody>
+      </table>
     )
   }
   return(
