d5f33e7c5ea7c7758f44a2f5f667bb2d5846c565
stefan <stefan.ylimaki@gmail.com>
2022-04-28 16:23:56 +0300

    2.11 done

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,22 +1,26 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
+import axios from 'axios'
 import Filter from "./components/Filter"
 import PersonForm from "./components/PersonForm"
 import Persons from "./components/Persons"
 
 const App = () => {
   // initializing persons state-variable and settings state to an array containing "Arto Hellas".
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   // initializing newName state-variable and setting state to empty string.
   const [newName, setNewName] = useState("")
   // initializing newNumber state-variable and setting state to empty string.
   const [newNumber, setNewNumber] = useState("")
   // initializing newSearch state-variable and setting state to empty string.
   const [newSearch, setNewSearch] = useState("")
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   /* onSubmit event handler for the HTML form's "add" button.
    * @Param event
